e7fb786f84a7f29e47c30e654fd04c6cfb7d711b
Djordje Ilic <djordjeilic555@gmail.com>
2017-07-03 15:31:59 +0200

    Napisane funkcije za proveru slobodnog termina rasporeda

--- Raspored/Raspored/Termin.cs
+++ Raspored/Raspored/Termin.cs
@@ -11,16 +11,19 @@ namespace Raspored
         public int Trajanje { get; set; }
         public DateTime Pocetak { get; set; }
         public int Broj { get; set; }
+        public Ucionica Ucionica { get; set; }
 
-        public Termin(int broj, int trajanje, DateTime pocetak)
+        public Termin(int broj, int trajanje, DateTime pocetak, Ucionica ucionica)
         {
             this.Broj = broj;
             this.Trajanje = trajanje;
             this.Pocetak = pocetak;
+            this.Ucionica = ucionica;
         }
-        public void edit(DateTime pocetak)
+        public void edit(DateTime pocetak, Ucionica ucionica)
         {
             this.Pocetak = pocetak;
+            this.Ucionica = ucionica;
         }
     }
 }
--- Raspored/Raspored/Termini.cs
+++ Raspored/Raspored/Termini.cs
@@ -10,25 +10,63 @@ namespace Raspored
     {
         public List<Termin> Podaci { get; set; }
         private int counter = 1;
-        public bool add(int broj, int trajanje, DateTime pocetak)
+        public bool add(int broj, int trajanje, DateTime pocetak,Ucionica ucionica)
         {
             if (Podaci.First(item => item.Broj == broj) != null)
             {
-                Podaci.Add(new Termin(broj, trajanje, pocetak));
+
+                Podaci.Add(new Termin(broj, trajanje, pocetak, ucionica));
                 return true;
             }
             return false;
         }
-        public void edit(int broj, DateTime pocetak)
+        public void edit(int broj, DateTime pocetak, Ucionica ucionica)
         {
             Termin s = Podaci.First(item => item.Broj == broj);
-            s.edit(pocetak);
+            s.edit(pocetak,ucionica);
             return;
         }
         public void remove(int broj)
         {
             Podaci.Remove(Podaci.First(item => item.Broj == broj));
             return;
+        }
+        public bool istekao(DateTime vreme)
+        {
+            if (DateTime.Now.CompareTo(vreme)<=0)
+                return true;
+            else
+                return false;
+        }
+        public bool slobodan(DateTime vreme)
+        {
+            DateTime endtime;
+            foreach (Termin podatak in Podaci)
+            {
+                endtime = podatak.Pocetak.AddHours((double)(podatak.Trajanje));
+                if (podatak.Pocetak.CompareTo(vreme) <= 0 && endtime.CompareTo(vreme)>=0)//vece od pocetka manje od kraja
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public bool slobodanZa(DateTime vreme, Termin termin)
+        {
+            DateTime pocetno = termin.Pocetak;
+            DateTime zavrsno = pocetno.AddHours(termin.Trajanje);
+            DateTime pocetnoPodatak;
+            DateTime zavrsnoPodatak;
+            foreach (Termin podatak in Podaci)
+            {
+                pocetnoPodatak = podatak.Pocetak;
+                zavrsnoPodatak = podatak.Pocetak.AddHours(podatak.Trajanje);
+                if (pocetnoPodatak <= zavrsno && zavrsnoPodatak >= pocetno)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
