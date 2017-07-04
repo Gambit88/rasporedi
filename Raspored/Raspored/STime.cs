f20b0a230159115a892cbc4651787d6fd8b459e4
Djordje Ilic <djordjeilic555@gmail.com>
2017-07-04 09:09:39 +0200

    Dodata klasa za deo rasporeda

---
+++ Raspored/Raspored/STime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raspored
+{
+    class STime
+    {
+        public DateTime Vreme {get;set;}
+        public bool Zauzeto { get; set; }
+        public bool Prvo { get; set; }
+        public bool Poslednje { get; set; }
+        public bool Aktivno { get; set; }
+    }
+}
